d99a13c037af136c2aa651f438e3af18297f76b9
Kota Karthikeya Raj <24311a04eg@ece.sreenidhi.edu.in>
2026-05-25 19:50:48 +0530

    Fix formatting and add newlines in app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return jsonify({"message": "Hello CI/CD"}), 200
 
+
 @app.route("/health")
 def health():
     return jsonify({"status": "healthy"}), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
